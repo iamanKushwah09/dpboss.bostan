073fd9c21f45d9a45c82016ffd9d6ed9ade8506c
Aman Kushwah <amankushwah073@gmail.com>
2026-06-05 12:38:16 +0530

    feat: implement API endpoints to fetch and save application settings using jsonblob

--- api/getSettings.js
+++ api/getSettings.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const response = await fetch("https://jsonblob.com/api/jsonBlob/019e8e65-73ad-76d9-89d0-58feb69ffdb0");
     if (!response.ok) throw new Error("Failed to fetch");
